e0fd736ba41fea2555065c6f6b2f5431e4f0b112
ReturnSweetPork <happylovetkd@naver.com>
2019-12-20 11:02:03 +0900

    App.js change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          여기가 바로 바뀌는 라인이다.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>안녕하세요 테스트중입니다</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
